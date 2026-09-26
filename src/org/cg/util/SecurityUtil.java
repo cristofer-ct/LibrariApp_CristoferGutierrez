ff2fcd964c655d4de8f6dbf8d11bb7b5cf58f617
cristofer guitorieres <cgutierrez-2026120@kinal.edu.gt>
2026-09-25 18:21:30 -0600

    feat: javadoc SecurityUtil

--- src/org/cg/util/SecurityUtil.java
+++ src/org/cg/util/SecurityUtil.java
@@ -10,6 +10,13 @@ import java.security.NoSuchAlgorithmException;
  * @author Cristofer
  * @version 1.0.0
  */
+/**
+ * Class SecurityUtil del sistema LibraryApp.
+ *
+ * @author Cristofer Gutierrez
+ * @version 1.0.0
+ * @see <a href='https://github.com/cristofer-ct/LibrariApp_CristoferGutierrez'>Repositorio</a>
+ */
 public class SecurityUtil {
     
     /**
@@ -21,6 +28,11 @@ public class SecurityUtil {
      * Método hashSHA256.
      * @param password parámetro de tipo String
      * @return un valor de tipo String
+     */
+    /**
+     * Ejecuta la operacion hashSHA256.
+     * @param password valor de tipo String
+     * @return valor de tipo String
      */
     public static String hashSHA256(String password){
         try {

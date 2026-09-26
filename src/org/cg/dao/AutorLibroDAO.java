c5fd01f571098bfdb84bb16807947abd7fcdffcd
cristofer guitorieres <cgutierrez-2026120@kinal.edu.gt>
2026-09-25 18:21:42 -0600

    feat: javadoc AutorLibroDAO

--- src/org/cg/dao/AutorLibroDAO.java
+++ src/org/cg/dao/AutorLibroDAO.java
@@ -8,6 +8,13 @@ import org.cg.model.AutorLibro;
  * @author Cristofer
  * @version 1.0.0
  */
+/**
+ * Interface AutorLibroDAO del sistema LibraryApp.
+ *
+ * @author Cristofer Gutierrez
+ * @version 1.0.0
+ * @see <a href='https://github.com/cristofer-ct/LibrariApp_CristoferGutierrez'>Repositorio</a>
+ */
 public interface AutorLibroDAO extends Crud<AutorLibro, Integer>{
     
 }

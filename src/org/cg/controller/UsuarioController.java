3a339a6ab60a307d26e1c868096f56e48f537d71
cristofer guitorieres <cgutierrez-2026120@kinal.edu.gt>
2026-09-25 18:23:30 -0600

    feat: javadoc UsuarioController

--- src/org/cg/controller/UsuarioController.java
+++ src/org/cg/controller/UsuarioController.java
@@ -35,6 +35,13 @@ import org.cg.util.SecurityUtil;
  * @author Cristofer
  * @version 1.0.0
  */
+/**
+ * Class UsuarioController del sistema LibraryApp.
+ *
+ * @author Cristofer Gutierrez
+ * @version 1.0.0
+ * @see <a href='https://github.com/cristofer-ct/LibrariApp_CristoferGutierrez'>Repositorio</a>
+ */
 public class UsuarioController implements Initializable {
 
     @FXML
@@ -103,6 +110,11 @@ public class UsuarioController implements Initializable {
      * Método initialize.
      * @param location parámetro de tipo URL
      * @param resources parámetro de tipo ResourceBundle
+     */
+    /**
+     * Ejecuta la operacion initialize.
+     * @param location valor de tipo URL
+     * @param resources valor de tipo ResourceBundle
      */
     public void initialize(URL location, ResourceBundle resources) {
         cmbRol.setItems(FXCollections.observableArrayList("admin", "empleado", "cajero"));

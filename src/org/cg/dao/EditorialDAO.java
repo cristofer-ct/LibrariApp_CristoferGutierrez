ec02caa832bb3b5795a1d2f81dadd634eedd463d
cristofer guitorieres <cgutierrez-2026120@kinal.edu.gt>
2026-09-25 18:21:54 -0600

    feat: javadoc EditorialDAO

--- src/org/cg/dao/EditorialDAO.java
+++ src/org/cg/dao/EditorialDAO.java
@@ -10,6 +10,13 @@ import org.cg.model.Editorial;
  *
  * @author aruba
  */
+/**
+ * Interface EditorialDAO del sistema LibraryApp.
+ *
+ * @author Cristofer Gutierrez
+ * @version 1.0.0
+ * @see <a href='https://github.com/cristofer-ct/LibrariApp_CristoferGutierrez'>Repositorio</a>
+ */
 public interface EditorialDAO extends Crud<Editorial, String>{
     
 }

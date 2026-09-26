a77835dc13ceaada2c3521c8596300a5dfb3b273
cristofer guitorieres <cgutierrez-2026120@kinal.edu.gt>
2026-09-25 18:21:45 -0600

    feat: javadoc CategoriaDAO

--- src/org/cg/dao/CategoriaDAO.java
+++ src/org/cg/dao/CategoriaDAO.java
@@ -10,6 +10,13 @@ import org.cg.model.Categoria;
  *
  * @author aruba
  */
+/**
+ * Interface CategoriaDAO del sistema LibraryApp.
+ *
+ * @author Cristofer Gutierrez
+ * @version 1.0.0
+ * @see <a href='https://github.com/cristofer-ct/LibrariApp_CristoferGutierrez'>Repositorio</a>
+ */
 public interface CategoriaDAO extends Crud<Categoria, Integer>{
     
 }

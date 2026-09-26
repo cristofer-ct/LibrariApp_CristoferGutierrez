711b1976be9b2699358b08aac4d4d942f5d68132
cristofer guitorieres <cgutierrez-2026120@kinal.edu.gt>
2026-09-25 18:21:36 -0600

    feat: javadoc Crud

--- src/org/cg/dao/Crud.java
+++ src/org/cg/dao/Crud.java
@@ -8,6 +8,13 @@ import java.util.ArrayList;
  * @author Cristofer
  * @version 1.0.0
  */
+/**
+ * Interface Crud del sistema LibraryApp.
+ *
+ * @author Cristofer Gutierrez
+ * @version 1.0.0
+ * @see <a href='https://github.com/cristofer-ct/LibrariApp_CristoferGutierrez'>Repositorio</a>
+ */
 public interface Crud<T, K> {
     boolean crear(T entidad);
     boolean actualizar(T entidad);

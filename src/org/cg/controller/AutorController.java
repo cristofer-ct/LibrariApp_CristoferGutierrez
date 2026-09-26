fdc5dcf90edf02e93c5396e1206fc29eb6a38eb6
cristofer guitorieres <cgutierrez-2026120@kinal.edu.gt>
2026-09-25 18:22:48 -0600

    feat: javadoc AutorController

--- src/org/cg/controller/AutorController.java
+++ src/org/cg/controller/AutorController.java
@@ -28,6 +28,13 @@ import org.cg.system.Principal;
  * @author Cristofer
  * @version 1.0.0
  */
+/**
+ * Class AutorController del sistema LibraryApp.
+ *
+ * @author Cristofer Gutierrez
+ * @version 1.0.0
+ * @see <a href='https://github.com/cristofer-ct/LibrariApp_CristoferGutierrez'>Repositorio</a>
+ */
 public class AutorController implements Initializable {
 
     @FXML
@@ -78,6 +85,11 @@ public class AutorController implements Initializable {
      * Método initialize.
      * @param location parámetro de tipo URL
      * @param resources parámetro de tipo ResourceBundle
+     */
+    /**
+     * Ejecuta la operacion initialize.
+     * @param location valor de tipo URL
+     * @param resources valor de tipo ResourceBundle
      */
     public void initialize(URL location, ResourceBundle resources) {
         cargarTabla();
